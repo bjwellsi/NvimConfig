9dd04fc44e65125da58bd5139a8536f43649d40d
Braiden <braidenjwells@gmail.com>
2025-04-24 19:16:04 -0500

    configured some shortcuts

--- lua/braiden/core/keymaps.lua
+++ lua/braiden/core/keymaps.lua
@@ -3,19 +3,18 @@ vim.g.mapleader = " "
 local keymap = vim.keymap --for conciseness
 
 --general keymaps
---keymap.set("i", "jk", "<ESC>")
+keymap.set("i", "jk", "<ESC>") --exit insert with jk
+
+keymap.set("n", "<leader>q", ":NvimTreeClose<CR>:q<CR>") -- close tree, then quit
 
 keymap.set("n", "<leader>nh", ":nohl<CR>") --clear search highlighting
 
 keymap.set("n", "x", '"_x') --don't copy x into register
 
-keymap.set("n", "<leader>+", "<C-a>") --increment #
-keymap.set("n", "<leader>-", "<C-x>") --decrement #
-
---keymap.set("n", "<leader>sv", "<C-w>v")--split window vertically
+keymap.set("n", "<leader>sv", "<C-w>v") --split window vertically
 --keymap.set("n", "<leader>sh", "<C-w>s")--split window horizontally
 --keymap.set("n", "<leader>se", "<C-w>=")--make split windows equal width
---keymap.set("n", "<leader>sx", ":close<CR>")--close current split window
+keymap.set("n", "<leader>sx", ":close<CR>") --close current split window
 
 --keymap.set("n", "<leader>to", ":tabnew<CR>")--open new tab
 --keymap.set("n", "<leader>tx", ":tabclose<CR>")--close current tab
@@ -32,5 +31,5 @@ keymap.set("n", "<leader>w", ":NvimTreeFocus<CR>") --change the toggle command
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
 keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
-keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+--keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
