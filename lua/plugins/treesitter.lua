8d10b4a64ae08c48a90081286a582fc359ce4c6c
braiden <braiden.wells@stellify.com>
2025-06-17 12:29:38 -0500

    turning off treesitter for large strings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,37 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
+		local function disable_function(lang, buf)
+			local max_string_chars = 40000
+			local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+
+			local in_string = false
+			local current_string = ""
+
+			for _, line in ipairs(lines) do
+				for c in line:gmatch(".") do
+					if c == '"' then
+						if in_string then
+							if #current_string > max_string_chars then
+								vim.schedule(function()
+									vim.notify(
+										"Disabling Tree-sitter highlight: huge string literal detected",
+										vim.log.levels.WARN
+									)
+								end)
+								return true
+							end
+							current_string = ""
+						end
+						in_string = not in_string
+					elseif in_string then
+						current_string = current_string .. c
+					end
+				end
+			end
+
+			return false
+		end
 		require("nvim-treesitter.configs").setup({
 			ensure_installed = {
 				"json",
@@ -20,7 +51,7 @@ return {
 				"dockerfile",
 				"gitignore",
 			},
-			highlight = { enable = true },
+			highlight = { enable = true, disable = disable_function },
 			indent = { enable = true },
 			autotag = { enable = true },
 		})
